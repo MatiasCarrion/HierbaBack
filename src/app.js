b65f74ef0a6d86bddef2012d23b47ff578e3ce04
MatiasCarrion <matiasezequielcarrion@gmail.com>
2021-07-27 17:35:02 -0300

    cambiando extended a true en bodyparser

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const jwt = require('jsonwebtoken');
 //body parser
 const bodyParser = require('body-parser')
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: true }))
 // parse application/json
 app.use(bodyParser.json())
 
