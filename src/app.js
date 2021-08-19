d3280effb170793c8f5fe746ae62d8d04d5cfdd1
MatiasCarrion <matiasezequielcarrion@gmail.com>
2021-08-18 21:02:01 -0300

    agregando los headers

--- src/app.js
+++ src/app.js
@@ -17,6 +17,15 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // parse application/json
 app.use(bodyParser.json())
 
+// para que permita peticiones
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 // configuramos las rutas
 app.use('/api/stock', require('./routes/stock'));
 app.use('/api/user', require('./routes/user'));
